ae84c7fd06820d7afb71edcaa903c8503e2ec8fa
arshak <arshakr10@gmail.com>
2019-10-03 19:30:42 +0300

    Fool

--- Program.cs
+++ Program.cs
@@ -14,13 +14,31 @@ namespace OOP.Person
         public int Weigth { get; set; } 
         public int Tariq { get; set; } 
         public int  Location { get; set; }
-
-
-
+        public bool Talk
+        {
+            set
+            {
+                if (value == true)
+                {
+                    for (int i = 0; i <= 5; i++)
+                    {
+                        Console.Beep();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Yes Arshak talks not too much");
+                }
+            }
+            get
+            {
+                return Talk;
+            }
+        }
 
         public void Walk(int distance)
         {
-            this.Location--;
+            this.Weigth--;
         }
         public void Eat(int weigth)
         {
@@ -33,8 +51,6 @@ namespace OOP.Person
        public void Runing( int Kilometrs)
         {
            this.Weigth--;
-
-
         }
     }
     class Program
@@ -44,17 +60,22 @@ namespace OOP.Person
             Person human = new Person();
             human.Name = "Arshak";
             human.LastName = "Lazaryan";
+            Console.WriteLine($"My Name Is {human.Name} {human.LastName}");
             human.Weigth = 92;
+            Console.WriteLine($"My Weigth is {human.Weigth}");
             human.Location = 10;
             //human.Tariq = new DateTime(1995,10, 2)  ;
-            Console.WriteLine(human.Full_Name());
-            human.Eat(10);
-            Console.WriteLine(human.Weigth);
-            human.Walk(25);
-            Console.WriteLine(human.Weigth);
+            //nsole.WriteLine(human.Full_Name());
+            human.Eat(1);
+            Console.WriteLine($"I Have Eaten And My Weigth is {human.Weigth}");
+            human.Walk(50);
+            Console.WriteLine($"I Walked And My Weigth is {human.Weigth} Now");
             human.Runing(10);
             Console.WriteLine(human.Weigth);
-
+            Console.WriteLine("If you agree that Arshak talks much whrite true else false");
+            human.Talk = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine(human.Talk);
+            
             
         
 
